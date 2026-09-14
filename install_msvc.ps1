a9d36ea1ca5952eb50635e995b1cd92cc2e67a32
Sjoerd Wouters <59654421+sjoerdev@users.noreply.github.com>
2026-09-14 03:36:18 -0700

    Update install_msvc.ps1

--- install_msvc.ps1
+++ install_msvc.ps1
@@ -1,12 +1,25 @@
 # installs visual studio build tools and the msvc compiler
 
-$wingetArguments = 
-@(
-    "--silent",
-    "--accept-package-agreements",
-    "--accept-source-agreements",
-    "--disable-interactivity",
+$wingetArguments = @(
+    "--silent"
+    "--accept-package-agreements"
+    "--accept-source-agreements"
+    "--disable-interactivity"
     "--force"
 )
 
-winget install --id "Microsoft.VisualStudio.BuildTools" @wingetArguments --override "--add Microsoft.VisualStudio.Workload.VCTools --includeRecommended --quiet"
+$vsComponents = @(
+    "Microsoft.VisualStudio.Workload.VCTools"
+    "Microsoft.VisualStudio.Component.VC.Tools.x86.x64"
+    "Microsoft.VisualStudio.Component.Windows11SDK.26100"
+)
+
+$vsOverrideArgs = @(
+    $vsComponents | foreach { "--add" $_ }
+    "--quiet"
+    "--wait"
+    "--norestart"
+)
+$vsOverride = $vsOverrideArgs -join " "
+
+winget install --id "Microsoft.VisualStudio.BuildTools" @wingetArguments --override $vsOverride
